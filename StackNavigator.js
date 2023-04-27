ffb3701c5eba3e0d20708dacf21aa24d95c26184
Caleb Love <mrcaleblove@live.com>
2023-04-27 17:58:12 +1000

    first screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import HomeScreen from './screens/HomeScreen'
+import ChatScreen from './screens/ChatScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -8,7 +9,7 @@ const StackNavigator = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Chat" component={ChatScreen} />
         </Stack.Navigator>
     )
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+    const navigation = useNavigation()
   return (
     <View>
       <Text>I am the HomeScreen</Text>
+      <Button title="Go to chat Screen" onPress={() => navigation.navigate("Chat") } />
     </View>
   )
 }
